2cc6f83bed7d9a576d8ef38672d1465644f2763d
Tim <tbajmat@gmail.com>
2016-05-12 09:56:40 +0200

    Added Project to branch

---
+++ MatchMakerVR/Assets/Scripts/Animations.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations : MonoBehaviour {
+
+    //list of event objects
+    [SerializeField]
+    private GameObject animationObject_1;
+
+    //animations from the objects
+    private Animator animation_1;
+
+
+    private bool intersectionEvent = false;
+
+    private float up = 0.2f;
+	// Use this for initialization
+	void Start () {
+        animation_1 = animationObject_1.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Animation_1();
+	}
+
+    private void Animation_1()
+    {
+        animation_1.SetBool("Play", true);
+    }
+}
+
---
+++ MatchMakerVR/Assets/Waypoints.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoints : MonoBehaviour {
+    [SerializeField]
+    private Transform[] Points;
+    [SerializeField]
+    private Transform CurrentWaypoint;
+    [SerializeField]
+    private float MoveSpeed = 10f;
+    [SerializeField]
+    private float Damping = 3f;
+    [SerializeField]
+    private Transform MyTransform;
+    [SerializeField]
+    private int index = 0;
+    // Use this for initialization
+    void Start() {
+        CurrentWaypoint = Points[0];
+    }
+
+    // Update is called once per frame
+    void Update() {
+        MoveTowards();
+    }
+
+    void LateUpdate()
+    {
+        //Quaternion rotation = Quaternion.LookRotation(CurrentWaypoint.position - MyTransform.position);
+        //MyTransform.rotation = Quaternion.Slerp(MyTransform.rotation, rotation, Time.deltaTime * Damping);
+    }
+
+    void MoveTowards()
+    {
+        float step = MoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, CurrentWaypoint.position, step);
+        if (transform.position == CurrentWaypoint.position)
+        {
+            if (index == Points.Length)
+            {
+                index = 0;
+            }            
+            CurrentWaypoint = Points[index];
+            index++;
+        }
+    }
+}
