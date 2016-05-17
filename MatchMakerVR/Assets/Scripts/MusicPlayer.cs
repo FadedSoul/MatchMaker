7bb722932499cf6552fa33b0aaca2b450f229c3f
Tim <tbajmat@gmail.com>
2016-05-17 22:32:33 +0200

    Audio Test

---
+++ MatchMakerVR/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public delegate void finishedPlaying();
+	public static event finishedPlaying OnFinishedPlaying;
+
+	AudioSource audioSource;
+
+	void OnEnable()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update()
+	{
+		if(!audioSource.isPlaying){
+			if(OnFinishedPlaying != null){
+				OnFinishedPlaying();
+			}
+		}
+	}
+}
